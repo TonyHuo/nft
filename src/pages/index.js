0ee967b22743645e04ae8a16a694becc76ae74cd
Stanley <aaa@ss.com>
2021-12-10 17:02:40 +0800

    change layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,22 @@
+import { withArtDirection } from "gatsby-plugin-image"
 import * as React from "react"
 import logo from "../images/pixelbitshot.png"
-import "../styles/global.css"
+import  "../styles/global.css"
 // styles
 const pageStyles = {
   
   background: "#000000",
   padding: 26,
   fontFamily: "-apple-system, Roboto, sans-serif, serif",
+}
+const contentStyles = {
+  
+    margin: "auto",
+    width: "50%",
+    border: "3px solid green",
+    padding: 10,
+    color:"#ffffff",
+    
 }
 const headingStyles = {
 
@@ -57,6 +67,7 @@ const descriptionStyle = {
   marginTop: 10,
   marginBottom: 0,
   lineHeight: 1.25,
+  
 }
 
 const docLink = {
@@ -81,66 +92,28 @@ const badgeStyle = {
   lineHeight: 1,
 }
 
-// data
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
 
 // markup
 const IndexPage = () => {
   return (
+  
     <main style={pageStyles}>
       <title>Pixel Bigshot</title>
-      <h1 style={headingStyles}>
-       
+      <h1 style={headingStyles}> 
         <img src={logo} alt="title" width="100%" height="100%"></img>
         <span role="img" aria-label="Party popper emojis">
-          🎉🎉🎉
         </span>
       </h1>
-      
+      <div style={contentStyles}><img src={"https://via.placeholder.com/100"} alt="title" width="100" height="100"></img></div>
+      <div style={contentStyles}>About this project</div>
+      <div style={contentStyles}> 
+        Mint: <input type="number" id="fname" name="fname"></input>
+      </div>
+      <div style={contentStyles}> 
+        <button>Confirm</button>
+      </div>
+
+
     </main>
   )
 }
