da4fd7542924ba18acf62fa89184f41f821b588c
Stanley <aaa@ss.com>
2021-12-22 16:00:24 +0800

    add meta

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,7 @@ module.exports = {
     title: "nft",
   },
   plugins: [
+    `gatsby-plugin-react-helmet`,
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import App from './App'
-
+import { Helmet } from "react-helmet"
 
 // Step 2: Define your component
 class IndexPage extends Component {
@@ -9,8 +9,16 @@ class IndexPage extends Component {
     render(){
       return (
         <main>
-            <title>Pixel Bigshot</title>
-          
+            <div className="application">
+                <Helmet>
+                <meta charSet="utf-8" />
+                <title>Pixel Bigshot</title>
+                <meta property="og:title" content="Pixel Bigshot" />
+                <meta property="og:type" content="100 Pixel Bigshot (PBS) in the world." />
+                <meta property="og:url" content="https://pixelbigshot.com" />
+                <meta property="og:image" content="https://www.pixelbigshot.xyz/static/pixel-7424a8e7cd2c5ac3b632d192517fe7a5.gif" />
+                </Helmet>
+            </div>
             <div>
                 <App></App>
             </div>
